581c9679603ddf4e1b3020379594d3f10f806b7e
Is an English Major <Najeebihesa@gmail.com>
2025-07-29 16:49:48 +0300

    still figuring out login stuff

--- server.js
+++ server.js
@@ -4,21 +4,31 @@ const dotenv = require('dotenv').config()
 const methodOverride = require("method-override")
 const connectToDB = require("./config/db")
 const backlogRoutes = require('./routes/backlog.routes')
+const authRoutes = require('./routes/auth.routes')
 const morgan = require("morgan")
-
+const session = require("express-session")
+const passUserToView = require('./middleware/passUserToView')
+const isSignedIn = require("./middleware/isSignedIn")
 
 // middleware
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride("_method"))
 app.use(morgan('dev'))
-
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passUserToView)
 // database connection 
 connectToDB()
 
 // routes go here
 app.use('/backlog', backlogRoutes)
-
+app.use('/auth', authRoutes )
 
 
 
